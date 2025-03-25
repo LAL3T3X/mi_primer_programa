f81696d697b23df30b6139d3f5e3bb5234aa21a2
LAL3T3X <eduardo.ap@ucad.edu.mx>
2025-03-25 13:31:45 -0600

    Nueva linea

--- Edu nacho.cpp
+++ Edu nacho.cpp
@@ -5,12 +5,14 @@ using namespace std;
 int main() {
     ofstream archivo("musiquita.txt");
     
+    cout<<"Hola muñaño inicia el programa"<<endl;
+    
     archivo<<"Genero musical que me gusta: Rock\n";
     archivo<<"Mi cantante favorito: Freddie Mercury\n";
 
 
     cout<<"Datos escritos en el archivo correctamente."<<endl;
-
+	
     return 0;
 }
 
